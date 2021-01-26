d001560156e2257c686cb572a257c168dc3a59c3
angelia collins <angiecollins09@gmail.com>
2021-01-26 14:40:31 -0700

    database conversion to sequelize

---
+++ models/crew.js
@@ -0,0 +1,26 @@
+module.exports = function (sequelize, DataTypes) {
+  const Crew = sequelize.define("Matches", {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    attractionName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dateEntered: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    }
+
+  });
+  return Crew;
+};
