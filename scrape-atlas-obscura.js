c4836d68528ecce9971a36cbf7a410a6475e1ccf
angelia collins <angiecollins09@gmail.com>
2021-01-26 18:03:37 -0700

    make the scrape one function

--- scrape-atlas-obscura.js
+++ scrape-atlas-obscura.js
@@ -2,17 +2,17 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 let url = 'https://www.atlasobscura.com/things-to-do/';
 
-
-function buildURL (country, city = null, state = null){
-  if (city){ 
+function scrape() {
+function buildURL(country, city = null, state = null) {
+  if (city) {
     url = url + city + "-";
   };
-  if (state){
+  if (state) {
     country = state;
   };
   url = url + country + "/places?";
 };
-buildURL();
+buildURL("belgium");
 console.log(url);
 
 axios.get(url)
@@ -20,15 +20,18 @@ axios.get(url)
     // handle success
     const $ = cheerio.load(response.data, null, false);
     let attractions = [];
-    $('a.Card.--content-card-v2.--content-card-item.Card--flat').each(function(i, element){
-        const location = $(element).find('div.Card__hat.--place').text();
-        const name = $(element).find('h3.Card__heading.--content-card-v2-title').text();
-        const description = $(element).find('div.Card__content.js-subtitle-content').text();
-        const image = $(element).find('img').attr('data-src');
+    $('a.Card.--content-card-v2.--content-card-item.Card--flat').each(function (i, element) {
+      const location = $(element).find('div.Card__hat.--place').text();
+      const name = $(element).find('h3.Card__heading.--content-card-v2-title').text();
+      const description = $(element).find('div.Card__content.js-subtitle-content').text();
+      const image = $(element).find('img').attr('data-src');
 
-        let card = {location, name, description, image};
-        attractions.push(card);
+      let card = { location, name, description, image };
+      attractions.push(card);
     })
-
-console.log(attractions);
+    console.log(attractions);
   });
+};
+
+// scrape();
+module.exports = scrape();
