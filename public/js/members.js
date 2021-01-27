b9a28f711a6d78ea80876c8e2e904aa5dd134986
angelia collins <angiecollins09@gmail.com>
2021-01-27 14:30:34 -0700

    attempting a modal

--- public/js/members.js
+++ public/js/members.js
@@ -11,6 +11,32 @@ $(document).ready(() => {
     const search = $("input#searchbar").val().trim();
     
 console.log(search);
+ $.ajax({
+   method: "GET",
+   url: "/api/search/" + search
+ }).then(data => {
+   console.log(data)
+
+   for ( var i = 0; i < data.length; i++){
+   
+$(".places").append(
+   `
+   <div class="card text-center" style="width: 18rem;">
+       <img class="card-img-top"
+           src="${data[i].image}"
+           alt="Card image cap">
+       <div class="card-body">
+           <h5 class="card-title">${data[i].location}</h5>
+           <h6>${data[i].name}</h6>
+           <p class="card-text">${data[i].description}</p>
+           <a href="#" class="btn btn-primary">Add to List</a>
+           <a href="#" class="btn btn-primary bg-danger border-danger">Not Interested</a>
+       </div>
+   `
+   )
+   }
+ })
+
   });
 });
 
