c63fa7bc2ba846a7ea33c3619b9109584558c1a7
angelia collins <angiecollins09@gmail.com>
2021-01-27 18:07:42 -0700

    modal probs

--- public/js/members.js
+++ public/js/members.js
@@ -6,21 +6,42 @@ $(document).ready(() => {
     $(".member-name").text(data.email);
   });
 
+
+
+
+  $(document).on('show.bs.modal','#exampleModalCenter', function (event) {
+    console.log('hi');
+    console.log(event);
+    console.log(event.relatedTarget);
+    let placename = $(event.relatedTarget).data('placename'); 
+    console.log(placename);
+        // $(this).find('.modal-body input').val(placename)
+        $(this).find('#exampleModalLongTitle').val(placename)
+        
+  })
+
   $("form.searchAttractions").on("submit", event => {
     event.preventDefault();
     const search = $("input#searchbar").val().trim();
-    
-console.log(search);
- $.ajax({
-   method: "GET",
-   url: "/api/search/" + search
- }).then(data => {
-   console.log(data)
 
-   for ( var i = 0; i < data.length; i++){
-   
-$(".places").append(
-   `
+    console.log(search);
+    $.ajax({
+      method: "GET",
+      url: "/api/search/" + search
+    }).then(data => {
+      console.log(data)
+
+      //triggered when modal is about to be shown
+      // $('#exampleModalCenter').on('show.bs.modal', function (event) {
+      //   console.log("howdy");
+      //   let bookId = $(event.relatedTarget).data('bookid'); 
+      //   $(this).find('.modal-body input').val(bookId)
+      // });
+
+      for (var i = 0; i < data.length; i++) {
+
+        $(".places").append(
+          `
    <div class="card text-center" style="width: 18rem;">
        <img class="card-img-top"
            src="${data[i].image}"
@@ -29,13 +50,37 @@ $(".places").append(
            <h5 class="card-title">${data[i].location}</h5>
            <h6>${data[i].name}</h6>
            <p class="card-text">${data[i].description}</p>
-           <a href="#" class="btn btn-primary">Add to List</a>
-           <a href="#" class="btn btn-primary bg-danger border-danger">Not Interested</a>
+           <button type="button" class="btn btn-primary add-to-list" data-toggle="modal" data-book-id="my_id_value" data-target="#exampleModalCenter" data-placename="${data[i].name}">
+           Add to List</button>
        </div>
+
+       <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">You are adding ${$(data-placename)} to your list.</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
    `
-   )
-   }
- })
+   
+        )
+      }
+
+
+    });
+
 
   });
 });
